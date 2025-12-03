d4c28e988953065f38512bdb6cdeb02d99b02ee2
Leon de Klerk <leon.deklerk@fenetre.nl>
2025-12-03 11:29:37 +0100

    faet: add demo setup

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { createApp, defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    getTemplate() {
+      const app = createApp({})
+      return app.component('TestComponent', {
+        data: () => {
+          return {
+            msg: this.msg as string,
+          }
+        },
+        methods: {
+          getMessage(): string {
+            return this.msg
+          },
+        },
+        template: `<div></div>`,
+      })
+    },
+  },
+})
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
